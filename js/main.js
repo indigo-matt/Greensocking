46a4324acf31ed3364154aa543373bfe2f488fb8
Matt Michaels <indigo-matt>
2016-07-17 18:32:11 -0700

    testing as a trigger example

--- js/main.js
+++ js/main.js
@@ -1,47 +1,12 @@
 (function($) {
  
-  // Init ScrollMagic
-  var ctrl = new ScrollMagic.Controller({
 
-  	// Setting a trigger when it leaves
-	  globalSceneOptions: {
-
-	  	// Tells it when to trigger, could be onEnter or onCenter
-      triggerHook: 'onLeave'
-    }
-  });
- 
+	AOS.init();
 
 
-	// Create scene
-	$("section").each(function() {
-
-		// Define the var as the #id of each <section> being looped through
-    var name = $(this).attr('id');
-
-    new ScrollMagic.Scene({
-
-    	// defines 'this' <section> as a scene
-      triggerElement: this 
-    })
-    .setPin(this)  		// Pins 'this' <section> to the top of the browser
-    .addIndicators() 	// Set indicator for 'this'
-    .loglevel(3)			// Enabling logging
-    .addTo(ctrl);			// Adds each scene to the 'ctrl' controllor
-	});
-
-
-	// Get the window height into this var
-	var wh = window.innerHeight;
-
-	new ScrollMagic.Scene({
-	  offset: wh*3	// Multiplying the window height by 3
-	})
-	
-	.setClassToggle("section#four", "is-active") // Adds a class when active.
-	
-	.addTo(ctrl);
-	
+$(window).scroll( function(event) {
+    console.log( $(this).scrollTop() );
+});
 
 
 
