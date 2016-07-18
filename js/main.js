c9278fe49cbd2d5c99540a1d8952dffbd462abea
Matt Michaels <indigo-matt>
2016-07-17 19:51:28 -0700

    testing combination of rellax with AOS

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 (function($) {
  
-
+  // Trigger AOS
 	AOS.init();
 
+  // Debug logging
+  $(window).scroll( function(event) {
+      console.log( $(this).scrollTop() );
+  });
 
-$(window).scroll( function(event) {
-    console.log( $(this).scrollTop() );
-});
-
-
+  // Rellax
+  var rellax = new Rellax('.rellax');
 
 })(jQuery);
